7a92f73352947c51334a12eb946546c29125dc0a
David Atanda <ololadedavid15@gmail.com>
2023-07-10 09:01:56 +0100

    now getting the greeting

--- src/cadance/scripts/getGreeting.js
+++ src/cadance/scripts/getGreeting.js
@@ -1,4 +1,4 @@
-export const getGreeting = `import HelloWorld from 0x01
+export const getGreeting = `import HelloWorld from 0xde3de6a3c08d0f66
 
 pub fun main(): String {
     return HelloWorld.greeting
