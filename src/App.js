f9ca42a9a4f64b5f884a1e9cb7bf1ce31e4a6a87
David Atanda <ololadedavid15@gmail.com>
2023-07-11 20:51:32 +0100

    route config

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import * as fcl from '@onflow/fcl'
-import * as types from '@onflow/types'
-import { getGreeting } from './cadance/scripts/getGreeting';
-import { changeGreeting } from './cadance/transactions/changeGreeting';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import First from './pages/First';
 
-// 0xde3de6a3c08d0f66
-fcl.config()
-  .put('accessNode.api', 'https://access-testnet.onflow.org')
-  .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn')
-function App() {
-
-  const [user, setUser] = useState()
-  const clickGreeting = async ()=>{
-    const result = await fcl.send([
-      fcl.script(getGreeting)
-    ]).then(
-      fcl.decode
-    )
-    console.log(result)
-  }
-  const login = ()=>{
-    fcl.authenticate()
-    fcl.currentUser().subscribe(setUser)
-  }
-  const changeTheGreeting = async ()=>{
-    const transactionId = await fcl.send([
-      fcl.transaction(changeGreeting),
-      fcl.args([
-        fcl.arg("Goodbye!", types.String)
-      ]),
-      fcl.payer(fcl.currentUser), //curentUser or authz
-      fcl.proposer(fcl.currentUser),
-      fcl.authorizations([fcl.currentUser]),
-      fcl.limit(9999)
-    ]).then(fcl.decode)
-
-    console.log(transactionId)
-  }
-  const logout = ()=>{
-    fcl.unauthenticate()
-  }
-
+function App(){
   return (
-    <div className="App">
-      <h1>Flow Hackathon</h1>
-      <h1>Account Address: {user && user.addr ? user.addr: ''}</h1>
-      <button onClick={clickGreeting}>Get Greeting</button>
-      <button onClick={changeTheGreeting}>Change The Greeting</button>
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path='/'
+            element={<SignIn />}
+          />
+          <Route
+            path='/first' 
+            element={<First />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
