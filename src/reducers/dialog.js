66700a1ec61c9a60aea7e1b537d75c09004481e6
David Atanda <ololadedavid15@gmail.com>
2023-07-18 10:34:24 +0100

    reducers

--- src/reducers/dialog.js
+++ src/reducers/dialog.js
@@ -1,15 +1,24 @@
 const initState = {
     open: false,
-    steps: { one: true, two: false, three: false }
+    one: true, 
+    two: false, 
+    three: false,
+    four: false
 }
 
 const DialogReducer = (state=initState, action)=>{
     switch (action.type) {
         case 'changeState':
             return {...state, open: action.payload}
+        case 'one':
+            return {...state, one: true, two: false, three: false }
         case 'two':
             return {...state, two: true, one: false }
-    
+        case 'three':
+            return {...state, two: false, three: true}
+        case 'four':
+            return {...state, four: true, three: false}
+            
         default:
             return state;
     }
